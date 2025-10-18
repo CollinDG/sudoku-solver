fc5defc49c3ce4583397dc2b1c0f94193835d5a5
Collin Garrison <154558368+CollinDG@users.noreply.github.com>
2025-10-17 20:00:38 -0700

    Fix tried values not resetting for different guesses

--- main.py
+++ main.py
@@ -1,19 +1,5 @@
 import copy
 
-HARD_PUZZLE = [
-    [0,0,1, 0,6,2, 0,0,0],
-    [0,0,0, 3,0,0, 0,0,0],
-    [4,0,0, 8,1,0, 0,0,0],
-
-    [2,0,0, 0,0,3, 7,0,0],
-    [7,3,0, 1,2,0, 5,9,0],
-    [6,0,0, 4,9,0, 1,0,0],
-
-    [1,0,0, 0,0,5, 6,0,3],
-    [0,0,0, 0,0,8, 0,0,0],
-    [5,0,0, 9,3,0, 0,0,0],
-]
-
 def solve_deterministic(puzzle, options, row_used_nums, col_used_nums, box_used_nums):
     deterministic = True
     while deterministic:
@@ -44,7 +30,9 @@ def solve_deterministic(puzzle, options, row_used_nums, col_used_nums, box_used_
             return puzzle, False
 
 
-def solve(puzzle, tried_values={}):
+def solve(puzzle, tried_values=None):
+    if tried_values is None:
+        tried_values = {}
     options = {(i,j): list() for i in range(9) for j in range(9)}
     row_used_nums = [{n for n in row if n != 0} for row in puzzle]
     col_used_nums = [{row[i] for row in puzzle if row[i] != 0} for i in range(9)]
@@ -56,7 +44,7 @@ def solve(puzzle, tried_values={}):
             for i in range(row_start, row_start+3):
                 box_used_nums[box_num].update(puzzle[i][col_start:col_start+3])
             box_used_nums[box_num].discard(0)
-            box_num += 1
+            box_num += 1    
     
     puzzle, solved = solve_deterministic(puzzle, options, row_used_nums, col_used_nums, box_used_nums)
 
@@ -82,20 +70,50 @@ def solve(puzzle, tried_values={}):
                             new_puzzle = copy.deepcopy(puzzle)
                             new_puzzle[i][j] = option
                             try:
-                                return solve(new_puzzle, tried_values)
+                                return solve(new_puzzle, copy.deepcopy(tried_values))
                             except ValueError:
                                 pass
     
     raise ValueError("Sudoku is not solvable")
-            
 
-if __name__ == "__main__":
-    solved_puzzle = solve(HARD_PUZZLE)
+def main():
+    valid = False
+    while not valid:
+        puzzle = []
+        rows = input(f"Input puzzle as comma-separated integers with a slash between each row: ").split("/")
+        if len(rows) != 9:
+            print("Invalid puzzle: Must have 9 rows")
+            continue
+        
+        parse_ok = True
+        for row in rows:
+            try: 
+                nums = [int(num.strip()) for num in row.split(",")]
+                if len(nums) != 9:
+                    print("Invalid puzzle: each row must have 9 numbers")
+                    parse_ok = False
+                    break
+                elif any(num < 0 or num > 9 for num in nums):
+                    print("Invalid puzzle: numbers must be between 0 and 9")
+                    parse_ok = False
+                    break
+            except ValueError:
+                print("Invalid puzzle: Input puzzle as comma-separated one-digit integers with a slash between each row")
+                parse_ok = False
+            puzzle.append(nums)
+    
+        if parse_ok:
+            valid = True
+
+    solved_puzzle = solve(puzzle)
     print("\nSolved puzzle:\n")
     for i, line in enumerate(solved_puzzle):
         for start in range(0,9,3):
-            print('  '.join([str(line[i]) for i in range(start, start+3)]), end="   ")
+            print("  ".join([str(line[i]) for num in range(start, start+3)]), end="   ")
         if (i+1) % 3 == 0:
             print("\n")
         else:
-            print()
+            print()    
+
+if __name__ == "__main__":
+    main()
